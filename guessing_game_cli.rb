5b8101dba2aaec2a1ad6c79369730a1d2e008637
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-02 11:24:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ if input == "exit"
   puts "Goodbye!"
 elsif input != random_number.to_s.gsub('\n','')
   puts "Sorry! The computer guessed #{random_number}."
-else 
+else
   puts "You guessed the correct number!"
 end
 end
