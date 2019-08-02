79f1c055074d47ef2c9119aa7dbff01ed9f77b91
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-02 11:16:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ random_number = rand(1..6)
 input = gets.chomp
 if input == "exit"
   puts "Goodbye!"
-elsif input.to_i == random_number
-  puts "You guessed the correct number!"
+elsif input.to_i != random_number
+  puts "Sorry! The computer guessed #{random_number.to_s}"
 end
 end
